0c7d4d6985f39192f3d27bacad285f51314f106c
twhite06 <50536683+twhite06@users.noreply.github.com>
2019-10-09 01:23:32 -0400

    Update characters.h

--- characters.h
+++ characters.h
@@ -1,1 +1,9 @@
+*
+* Personal Project 1
+* Tim Gray (White)
+* 10/09/2019
+*
+    The Ultimate Cartoon Fanatic Showdown
+---------------------------------------------------
 
+Update 10/09/2019 ( TO DO )
